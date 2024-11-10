f18075c8ac61df48dcda894579ec10f84d3f4dc5
Fer Ochoa <ferochoao123@gmail.com>
2024-11-10 01:13:42 -0600

    Configurar conexión a JAWSDB

--- database.js
+++ database.js
@@ -1,15 +1,12 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',  
-  user: 'root',  
-  password: 'Alfebella753',  
-  database: 'proyectopanelsolar'
+// Usamos la variable de entorno JAWSDB_URL para la conexión
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error de conexión a la base de datos:', err);
+    return;
+  }
+  console.log('Conectado a la base de datos');
 });
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conexión a la base de datos establecida');
-});
-
-module.exports = connection;
