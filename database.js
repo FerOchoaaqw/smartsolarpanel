f12eabec775e610a2a32de63977642e468d59a84
Fer Ochoa <ferochoao123@gmail.com>
2024-11-10 11:53:35 -0600

    Conexión a MySQL en Railway configurada

--- database.js
+++ database.js
@@ -1,15 +1,18 @@
-const mysql = require('mysql2');
-
+const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',  
-  user: 'root',  
-  password: 'Alfebella753',  
-  database: 'proyectopanelsolar'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conexión a la base de datos establecida');
+connection.connect(err => {
+  if (err) {
+    console.error("Error de conexión a la base de datos MySQL:", err);
+    throw err;
+  }
+  console.log("Conectado a la base de datos MySQL!");
 });
 
 module.exports = connection;
