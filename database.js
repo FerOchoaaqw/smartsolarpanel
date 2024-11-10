21935216f0751128aa581d5203cdf83df330732a
Fer Ochoa <ferochoao123@gmail.com>
2024-11-10 11:34:38 -0600

    Ajuste de puerto para despliegue en Heroku

--- database.js
+++ database.js
@@ -1,12 +1,15 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-// Usamos la variable de entorno JAWSDB_URL para la conexión
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect(err => {
-  if (err) {
-    console.error('Error de conexión a la base de datos:', err);
-    return;
-  }
-  console.log('Conectado a la base de datos');
+const connection = mysql.createConnection({
+  host: 'localhost',  
+  user: 'root',  
+  password: 'Alfebella753',  
+  database: 'proyectopanelsolar'
 });
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Conexión a la base de datos establecida');
+});
+
+module.exports = connection;
